c9ac833b49152e6f7dd2be38711b7c6289f7eb15
AiAe <ayyayyedev@gmail.com>
2020-12-20 20:56:13 +0200

    Qua fetcher

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3008;
+
+const yaml = require('js-yaml');
+const fetch = require('node-fetch');
+
+app.get('/', (req, res) => {
+    res.send('/:map_id')
+});
+
+async function get(url) {
+    return fetch(url).then(response => {
+        return response.text();
+    });
+}
+
+app.use(express.static(__dirname + '/../'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/:map', async (req, res) => {
+    const mapId = req.params.map;
+
+    if(isNaN(mapId)) return res.send('Fuck u');
+
+    const resp = await get('https://api.quavergame.com/d/web/map/' + mapId);
+    return res.render('../index.html', {resp: JSON.stringify(yaml.safeLoad(resp))});
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})

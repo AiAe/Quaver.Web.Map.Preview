8f0a885f741a38389303b472dec186adb1c0fd65
kloi34 <53842237+kloi34@users.noreply.github.com>
2021-12-02 10:27:48 -0800

    Fixed 7 key black background overflow

--- objects/PlayField.js
+++ objects/PlayField.js
@@ -1,5 +1,5 @@
 function drawPlayField() {
-    const fieldWidth = (noteSize.width) * numLanes - (numLanes * borderSize);
+    const fieldWidth = (noteSize.width - borderSize) * numLanes;
     let draw = new PIXI.Graphics();
     draw.beginFill(0x000000);
     draw.drawRect(0, 0, fieldWidth, window.innerHeight);
